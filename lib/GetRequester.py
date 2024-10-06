8cce6e94517802933cbf097ff00549925b1ea5c6
Ngecha <ngechadennis@gmail.com>
2024-10-06 19:18:07 +0300

    assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,27 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        search_term='daniel'
+        search_term_formatted=search_term.replace('','+')
+        fields=["name", "occupation"]
+        fields_formatted=",".join(fields)
+        limit=1
+        
+        URL=f"https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json?name={search_term_formatted}&occupation={fields_formatted}&limit={limit}"
+        
+        response = requests.get(URL)
+        return response.content
 
     def load_json(self):
-        pass
+        search_term="daniel"
+        search_term_formatted=search_term.replace("","+")
+        fields=['name','occupation']
+        fields_formatted=','.join(fields)
+        limit=1
+        
+        URL=f"https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json?name={search_term_formatted}&occupation={fields_formatted}&limit={limit}"
+        print (URL)
+        response=requests.get(URL)
+        
+        return response.json()
+        
